2ce96fc8fb4d3da741c90d1ee60bafd16bc4264e
IgorLekuona <ilekuona.work@gmail.com>
2024-03-20 18:56:06 +0100

    Updated sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -2,10 +2,10 @@
 def call(boolean abort = false, abortPipeline) {
 	if (abortPipeline != true) {
 		echo 'Ejecución de las pruebas de calidad de código'
-		// withSonarQubeEnv(credentialsId: 'sonarqube-token') {
+		withSonarQubeEnv(credentialsId: 'sonarqube-token') {
 		// 	def scannerHome = tool name: 'SonarScanner'
 		// 	bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=practica2 -Dsonar.projectName=Practica2 -Dsonar.projectVersion=${BUILD_NUMBER}-practica2"
-		// }
+		}
 		timeout(time: 5, unit: 'MINUTES') {
 			waitForQualityGate abortPipeline: abort
 		}
